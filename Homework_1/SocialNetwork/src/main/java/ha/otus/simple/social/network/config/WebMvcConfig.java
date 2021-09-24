fa71e770114f205300d55485812b9b9269b1fd31
user <nva2003yandex.ru>
2021-09-24 19:56:42 +0300

    addResourceHandlers

--- Homework_1/SocialNetwork/src/main/java/ha/otus/simple/social/network/config/WebMvcConfig.java
+++ Homework_1/SocialNetwork/src/main/java/ha/otus/simple/social/network/config/WebMvcConfig.java
@@ -1,16 +1,25 @@
 package ha.otus.simple.social.network.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/", "/static/css");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
+    }
+
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
 
         registry.addViewController("/login").setViewName("login");
+
     }
 }
