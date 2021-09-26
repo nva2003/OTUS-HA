44345c87c7457ec6b513ad0554d9d760f4ee76df
user <nva2003yandex.ru>
2021-09-26 22:28:45 +0300

    add data

---
+++ Homework_1/DB/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('tyrselestion', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Tyrion', 'Lannister', 100, 'UNKNOWN', 'murder', 'fantasy'),
+       ('cersei', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Cersei', 'Lannister', 100, 'UNKNOWN', 'murder', 'fantasy'),
+       ('jaime', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Jaime', 'Lannister', 100, 'UNKNOWN', 'murder', 'fantasy');
+       ('stark', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Ned', 'Stark', 100, 'UNKNOWN', 'murder', 'fantasy');
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('sansa', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Sansa', 'Stark', 100, 'UNKNOWN', 'murder', 'fantasy');
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('aria', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Aria', 'Stark', 100, 'UNKNOWN', 'murder', 'fantasy');
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('john', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Jon', 'Snow', 100, 'UNKNOWN', 'murder', 'fantasy');
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('dayneris', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Daenerys','Daenerys', 100, 'UNKNOWN', 'murder', 'fantasy');
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('rob', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Rob', 'Stark', 100, 'UNKNOWN', 'murder', 'fantasy'),
+       ('tywin', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Tywin', 'Lannister', 100, 'UNKNOWN', 'murder', 'fantasy'),
+       ('brienne', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Brienne', 'Tarth', 100, 'UNKNOWN', 'murder', 'fantasy'),
+        ('lord', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Lord', 'Varys', 100, 'UNKNOWN', 'murder', 'fantasy');
+
+INSERT INTO users (username, password, first_name, last_name, age, sex, interest, city)
+VALUES
+       ('1', '$2a$10$Wt7kjHv25Do3D6M5Yl9tTO0uVf/XMcl4uvAqor80yDgeRp.t7tPl6', 'Lord', 'Varys', 100, 'UNKNOWN', 'murder', 'fantasy');
