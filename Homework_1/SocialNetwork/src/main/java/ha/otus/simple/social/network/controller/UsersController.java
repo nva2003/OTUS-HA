a1f04a29027057c5e3ccb127f09035152c8e5671
user <nva2003yandex.ru>
2021-10-19 21:10:38 +0300

    add  find form

--- Homework_1/SocialNetwork/src/main/java/ha/otus/simple/social/network/controller/UsersController.java
+++ Homework_1/SocialNetwork/src/main/java/ha/otus/simple/social/network/controller/UsersController.java
@@ -8,10 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +35,7 @@ public class UsersController {
     private Integer defaultPageSize;
 
     private static final String VIEWS_FIND_USER_FORM = "findUsers";
+    private static final String VIEWS_USER_LIST_FORM = "users";
 
     private final UserMapper userMapper;
 
@@ -39,7 +45,7 @@ public class UsersController {
         SysUser user = serverUtils.getUserFromSession(request);
         List<SysUser> users = userMapper.findOther(user.getUserId());
             model.addAttribute("users",users);
-        return "users";
+        return VIEWS_USER_LIST_FORM;
     }
 
     @GetMapping("/find")
@@ -47,5 +53,27 @@ public class UsersController {
         model.put("user", new SysUser());
         model.put("menu", "find users");
         return VIEWS_FIND_USER_FORM;
+    }
+
+    @PostMapping("/find")
+    public String processFindForm(@ModelAttribute("user") SysUser user, BindingResult result, Map<String, Object> model) throws UnsupportedEncodingException {
+
+        // find users by last name
+        //todo
+        List<SysUser> users = userMapper.findOther(user.getUserId());
+        if (users.isEmpty()) {
+            // no users found
+            model.put("msg", "Пользователь с заданами параметрами не найден");
+//            model.put("menu", "find users");
+            return VIEWS_FIND_USER_FORM;
+
+        } else {
+            // multiple users found
+            model.put("users",users);
+//            model.put("menu", "user list");
+
+
+            return VIEWS_USER_LIST_FORM;
+        }
     }
 }
