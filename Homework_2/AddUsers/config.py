85a7d0b8fbe9018889d52a1f32732401fa3d0584
user <nva2003yandex.ru>
2021-10-27 22:31:23 +0300

    add Docker ENV для переменных Python

--- Homework_2/AddUsers/config.py
+++ Homework_2/AddUsers/config.py
@@ -1,6 +1,12 @@
+import os
+
 dbconfig = {
-    'user': 'root',
-    'password': 'password',
-    'host': 'localhost',
-    'database': 'myDB'
+    'user': os.getenv('MYSQL_USER', 'root'),
+    'password': os.getenv('MYSQL_PASSWORD', 'password'),
+    'host': os.getenv('MYSQL_HOST', 'localhost'),
+    'database': os.getenv('MYSQL_DATABASE', 'myDB')
+    # 'user': os.environ['MYSQL_USER',],
+    # 'password': os.environ['MYSQL_PASSWORD'],
+    # 'host': os.environ['MYSQL_HOST'],
+    # 'database': os.environ['MYSQL_DATABASE']
 }
