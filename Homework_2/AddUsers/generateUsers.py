20c8c365593683bb4c55dd1a835d6ad136bbc170
user <nva2003yandex.ru>
2021-10-28 09:22:09 +0300

    add time

--- Homework_2/AddUsers/generateUsers.py
+++ Homework_2/AddUsers/generateUsers.py
@@ -1,8 +1,13 @@
 import names
 import generate_random_email_address
 import addUser
+import time
+from datetime import timedelta
 
 def countdown(n):
+    start_time = time.perf_counter()
+    start_time_gt = time.time()
+
     while n > 0:
         real_name = names.get_full_name()
         first_name, last_name = real_name.lower().split(' ')
@@ -15,6 +20,16 @@ def countdown(n):
         n -= 1
         print(n)
     print('Готово!')
+
+    end_time = time.perf_counter()
+
+    print(f"Elapsed time: {end_time - start_time:0.4f} seconds")
+
+    elapsed_time = time.time() - start_time_gt
+    print ( time.strftime('%H:%M:%S', time.gmtime(elapsed_time) ) )
+
+    td = timedelta(seconds = elapsed_time)
+    print('Time in hh:mm:ss:', td)
 
 
 countdown(1000000)
